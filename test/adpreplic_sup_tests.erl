41665dcb595a465f708b8f01a41fe59f4e458cf9
Amadeo Asco <aas@trifork.com>
2014-12-16 10:43:46 +0000

    Unitest for module adpreplic_sup

---
+++ test/adpreplic_sup_tests.erl
@@ -0,0 +1,46 @@
+%% =============================================================================
+%% EUnits for supervisour - SyncFree
+%% 
+%% @author Amadeo Asco
+%% @version 1.0.0
+%% @reference Project <a href="https://syncfree.lip6.fr/">SyncFree</a>
+%% @reference More courses at <a href="http://www.trifork.com">Trifork Leeds</a>
+%% @end
+%% =============================================================================
+
+
+-module(adpreplic_sup_tests).
+
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([]).
+
+
+-include_lib("eunit/include/eunit.hrl").
+
+
+%% ====================================================================
+%% Test functions
+%% ====================================================================
+
+initialise_test() ->
+    spawn(fun() ->
+         {ok, Pid} = adpreplic_sup:start_link(),
+         exit(Pid, shutdown)
+         end),
+    wait_for_death_or_get_event(),
+    ?assert(whatever()).
+
+wait_for_death_or_get_event() ->
+    receive
+        Response ->
+            ?assertEqual(any, Response)
+    after
+        500 ->
+            ?assert(whatever())
+    end.
+
+whatever() ->
+    true.
