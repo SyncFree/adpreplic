df299ed9f6aebb4133c9413c87afda409405af3c
Adrian Vladu <avladu@cloudbasesolutions.com>
2016-01-11 21:31:20 +0100

    Automated erlang/git/adpreplic setup and run
    Just run:
    
    bash fullpath/install-erlang.sh
    bash fullpath/update-git-repo.sh
    bash fullpath/restart-adpreplic.sh
    
    Configure your repository path/branch/head of choice, erlang directory
    and adpreplic directory in deploy/ubuntu-14-04/etc/adpreplic.config
    
    Make sure your user has administrator rights, as the scripts need to
    install git and crudini packages.

---
+++ deploy/ubuntu-14-04/scripts/update-git-repo.sh
@@ -0,0 +1,33 @@
+#!/!bin/bash
+set -e
+
+BASEDIR=$(dirname $0)
+CONFIG_PATH="${BASEDIR}/../etc/adpreplic.config"
+
+ADPREPLIC_DIRECTORY=$(crudini --get ${CONFIG_PATH} local adpreplic_dir)
+GIT_REPOSITORY=$(crudini --get ${CONFIG_PATH} git repository) 
+GIT_BRANCH=$(crudini --get ${CONFIG_PATH} git branch) 
+GIT_COMMIT=$(crudini --get ${CONFIG_PATH} git commit)
+
+sudo apt-get install git -y
+if [ ! -d "$ADPREPLIC_DIRECTORY" ]; then
+  sudo mkidr -p $ADPREPLIC_DIRECTORY
+  chmod -R 777 $ADPREPLIC_DIRECTORY
+  git clone $GIT_REPOSITORY $ADPREPLIC_DIRECTORY
+else
+  echo "Adpreplic Directory exists. Skip cloning..."
+fi
+
+pushd $ADPREPLIC_DIRECTORY
+
+git checkout $GIT_BRANCH
+git pull origin $GIT_BRANCH
+
+if [ "$GIT_COMMIT" = "last" ]; then
+    echo "Skipping git reset to commit..."
+else
+    git reset --hard $GIT_COMMIT
+fi
+
+popd
+
